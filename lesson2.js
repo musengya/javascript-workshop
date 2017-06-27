1e0853a3335db08c8d27cddf140ba0c1164bda57
Irene Mutua <mutuamirene.gmail.com>
2017-06-27 17:23:43 +0300

    Add lesson2

---
+++ lesson2.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+"use strict";
+
+const first = "Irene";
+const last = "Mutua";
+
+if ((first === "Irene") && (last === "Mutua")) {
+    document.write("hey good job buddy");
+}
