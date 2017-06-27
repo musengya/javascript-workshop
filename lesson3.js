b1cbae91e02edd7fd7498c696aa325f9b8bb8163
Irene Mutua <mutuamirene.gmail.com>
2017-06-27 17:24:57 +0300

    Add lesson3

---
+++ lesson3.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+"use strict";
+
+const girl = "Irene";
+
+switch (girl) {
+    case "Irene":
+        document.write("I love my son");
+        break;
+    case "Mary":
+        document.write("You are our pillar");
+        break;
+    default:
+        document.write("This is the default");
+}
